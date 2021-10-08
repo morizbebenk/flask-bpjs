dbf534ba56f07d8da175798c650273cc40252a81
Moriz <morizbebenk@gmail.com>
2021-10-08 15:35:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,191 @@
+## Flask BPJS
+
+Webservice yang digunakan untuk menangani proses dekripsi data response dari bridging BPJS Vclaim-Rest 1.1 (Encrypted Version).
+
+## Kebutuhan
+
+**Utama**
+
+- Python
+
+**Package**
+
+- Flask
+- lzstring
+- requests
+- flask_cors 
+- pycryptodome 
+- python-dotenv 
+
+## Virtual Environment
+
+Bagian ini opsional tetapi sangat disarankan untuk membuat virtual environment supaya ketika ada update package yang tidak support / bermasalah tidak akan mengganggu aplikasi lain.
+
+### Membuat Virtual Environment
+
+```bash
+python -m venv virtualenv
+```
+
+atau
+
+```bash
+python3 -m venv virtualenv
+```
+
+### Aktivasi Virtual Environment
+
+- **Windows**
+
+    ```bash
+    virtualenv\Scripts\activate
+    ```
+
+- **Bash**
+
+    ```bash
+    source virtualenv/bin/activate
+    ```
+
+### Instal Paket Python
+
+```bash
+pip install Flask lzstring requests flask_cors pycryptodome python-dotenv
+```
+
+## File .env
+- Salin file `.env.example`
+- Ubah nama file menjadi `.env`
+- Lengkapi data `HOST`, `CONSID` & `SECRET`
+- `Host Development` : `https://dvlp.bpjs-kesehatan.go.id/vclaim-rest-1.1/`
+- `Host Production` : `-`
+
+## Pengaturan
+
+### Setup aplikasi
+
+- **Windows**
+
+    ```bash
+    set FLASK_APP=app
+    ```
+
+- **Bash**
+
+    ```bash
+    export FLASK_APP=app
+    ```
+
+### Mode Debug
+
+- **Windows**
+
+    </> **Development :**
+
+    ```bash
+    set FLASK_ENV=development
+    ```
+
+    atau
+    ```bash
+    set FLASK_DEBUG=1
+    ```
+
+    </> **Production :**
+
+    ```bash
+    set FLASK_ENV=production
+    ```
+
+    atau
+    ```bash
+    set FLASK_DEBUG=0
+    ```
+
+- **Bash**
+
+    </> **Development :**
+
+    ```bash
+    export FLASK_ENV=development
+    ```
+
+    atau
+
+    ```bash
+    export FLASK_DEBUG=1
+    ```
+
+    </> **Production :**
+
+    ```bash
+    export FLASK_ENV=production
+    ```
+
+    atau
+
+    ```bash
+    export FLASK_DEBUG=0
+    ```
+
+## Menjalankan Aplikasi
+
+#### Cara Mudah
+
+```bash
+flask run
+```
+
+Secara default akan berjalan di `http://127.0.0.1:5000`.
+
+#### Custom host
+```bash
+flask run -h 0.0.0.0
+```
+
+Dapat diakses semua klien di jaringan yang sama.
+
+#### Custom port
+```bash
+flask run -p 8080
+```
+
+Berjalan di port 8080.
+
+## Cara Pakai
+
+
+#### Menggunakan environment credential
+
+`host`, `consid` dan `secret` mengambil data dari file `.env`.
+
+| Request | Konten | Nilai | Keterangan |
+| ------- | ------ | ----- | ---------- |
+| Host | `http://127.0.0.1:5000` | - | Menyesuaikan host dan port yang di jalankan |
+| Header | `Content-Type` | `application/json` | Opsional, jika body format json maka ini wajib |
+| Method | `POST` | - | Wajib POST |
+| Body | `url` | `referensi/poli/ana` (contoh) | Wajib, mengacu dokumentasi VClaim BPJS tanpa base url. Contoh : `referensi/poli/ana` |
+| Body | `method` | `GET` / `POST` / `PUT` / `DELETE` | Wajib, mengacu dokumentasi VClaim BPJS |
+| Body | `payload` | `{"request": {"t_sep": {"noSep": "0301R0011017V000007", "user": "Coba Ws"}}}` (contoh) | Opsional menyesuaikan rest api VClaim BPJS |
+
+#### Menggunakan header credential
+
+`host`, `consid` dan `secret` mengambil data dari header yang dikirimkan, jika menggunakan metode ini diwajibkan mengirim data header `x-host`, `x-consid` dan `x-secret` dengan lengkap, jika salah satu kosong atau tidak dikirim maka secara default akan menggunakan metode `environment credential` diatas.
+
+| Request | Konten | Nilai | Keterangan |
+| ------- | ------ | ----- | ---------- |
+| Host | `http://127.0.0.1:5000` | - | Menyesuaikan host dan port yang di jalankan |
+| Header | `Content-Type` | `application/json` | Opsional, jika body format json maka ini wajib |
+| Header | `x-host` | `https://dvlp.bpjs-kesehatan.go.id/vclaim-rest-1.1/` (host development) | Custom host |
+| Header | `x-consid` | `1234` | Custom consid |
+| Header | `x-secret` | `12345abcde` | Custom secret |
+| Method | `POST` | - | Wajib POST |
+| Body | `url` | `referensi/poli/ana` (contoh) | Wajib, mengacu dokumentasi VClaim BPJS tanpa base url. Contoh : `referensi/poli/ana` |
+| Body | `method` | `GET` / `POST` / `PUT` / `DELETE` | Wajib, mengacu dokumentasi VClaim BPJS |
+| Body | `payload` | `{"request": {"t_sep": {"noSep": "0301R0011017V000007", "user": "Coba Ws"}}}` (contoh) | Opsional menyesuaikan rest api VClaim BPJS |
+
+## Sumber Daya
+- https://dvlp.bpjs-kesehatan.go.id:8888/trust-mark
+
+## Lisensi
+- Aplikasi ini open source dengan lisensi [MIT](LICENSE).
---
+++ app.py
@@ -0,0 +1,155 @@
+from Crypto.Util.Padding import pad, unpad
+from flask import Flask, request, jsonify
+from dotenv import dotenv_values
+from Crypto.Cipher import AES
+from datetime import datetime
+from flask_cors import CORS
+import lzstring
+import requests
+import hashlib
+import base64
+import hmac
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+
+def setNoneIfEmpty(data):
+    if data == '':
+        return None
+
+    return data
+
+def decrypt(keys, encrypts):
+    decompress = None
+
+    if encrypts != None:
+        x = lzstring.LZString()
+        key_hash = hashlib.sha256(keys.encode('utf-8')).digest()
+
+        decryptor = AES.new(key_hash[0:32], AES.MODE_CBC, IV=key_hash[0:16])
+        plain = decryptor.decrypt(base64.b64decode(encrypts))
+        decompress = json.loads(x.decompressFromEncodedURIComponent(plain.decode('utf-8')))
+        
+    return decompress
+
+def rest_bpjs(consid, secret, url, method, payload, timestamp):
+    message = consid+"&"+timestamp
+    signature = hmac.new(bytes(secret,'UTF-8'),bytes(message,'UTF-8'), hashlib.sha256).digest()
+    encodeSignature = base64.b64encode(signature)
+
+    headers = {'X-cons-id': consid, 'X-timestamp': timestamp, 'X-signature': encodeSignature.decode('UTF-8'), 'Content-Type': 'Application/x-www-form-urlencoded','Accept': '*/*'}
+
+    if payload == '' and payload == None :
+        payload = 0
+    else:
+        payload = json.dumps(payload)
+    
+    if method == 'post':
+        if payload == 0:
+            res = requests.post(url, headers=headers)
+        else:
+            res = requests.post(url, data=payload, headers=headers)
+    elif method == 'put':
+        if payload == 0:
+            res = requests.put(url, headers=headers)
+        else:
+            res = requests.put(url, data=payload, headers=headers)
+    elif method == 'delete':
+        if payload == 0:
+            res = requests.delete(url, headers=headers)
+        else:
+            res = requests.delete(url, data=payload, headers=headers)
+    else:
+        if payload == 0:
+            res = requests.get(url, headers=headers)
+        else:
+            res = requests.get(url, data=payload, headers=headers)
+
+    return res
+
+@app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH'])
+def bridging():
+    if(request.method == 'POST'):
+        header_host = request.headers.get('x-host')
+        header_consid = request.headers.get('x-consid')
+        header_secret = request.headers.get('x-secret')
+
+        if setNoneIfEmpty(header_host) == None or setNoneIfEmpty(header_consid) == None or setNoneIfEmpty(header_secret) == None:
+            config = dotenv_values(".env")
+            host = config['HOST_BPJS']
+            consid = config['CONSID']
+            secret = config['SECRET']
+
+        else:
+            host = header_host
+            consid = header_consid
+            secret = header_secret
+
+        is_json = False
+
+        if request.headers.get('Content-Type') == 'application/json':
+            is_json = True
+
+        if is_json == False:
+            url = host + request.form.get('url')
+            method = request.form.get('method')
+            payload = request.form.get('payload')
+
+        else:
+            data_json = request.json
+            url = host + data_json['url']
+            method = data_json['method']
+            payload = data_json['payload']
+
+        timestamp = str(int(datetime.today().timestamp()))
+
+        if(not url or not method):
+            data = {
+                'metaData': {
+                    'code': 400,
+                    'message': "Pastikan untuk mengirim url, method dan/atau payload",
+                },
+                'response': None
+            }
+
+            return jsonify(data), 400
+
+        res = rest_bpjs(consid, secret, url, method, payload, timestamp)
+
+        if res.status_code != 404:
+            keys = consid + secret + timestamp
+            res = res.json()
+
+            data = {
+                'metaData': {
+                    'code': res['metaData']['code'],
+                    'message': res['metaData']['message'],
+                },
+                'response': decrypt(keys, res['response'])
+            }
+
+            return jsonify(data), res['metaData']['code']
+
+        else:
+            data = {
+                'metaData': {
+                    'code': 404,
+                    'message': "URL tidak ditemukan",
+                },
+                'response': None
+            }
+
+            return jsonify(data), 404
+    
+    else:
+        data = {
+            'metaData': {
+                'code': 405,
+                'message': "Method dilarang, gunakan method POST",
+            },
+            'response': None
+        }
+
+        return jsonify(data), 405
+    
